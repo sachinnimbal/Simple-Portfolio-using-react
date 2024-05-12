8ca34c2574e95e30664143738b881bf9e38e6aa4
Sachin Nimbal <87244512+sachinnimbal@users.noreply.github.com>
2024-05-12 09:39:53 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,3 @@
-import Food from "../images/ProjectImages/Food Delivery.png";
-import Invoice from "../images/ProjectImages/Invoicing.png";
-import SUK from "../images/ProjectImages/CIE Entry.png";
-import CIE from "../images/ProjectImages/SUKApp.jpg";
-import HOSTEL from "../images/ProjectImages/Hostel.png";
-
 export default function Portfolio() {
   return (
     <>
@@ -15,110 +9,111 @@ export default function Portfolio() {
           <div className="portfolio-section">
             <div className="card">
               <div className="card-image-container">
-                <img src={Food} alt="FOOD DELIVERY WEB APP" />
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
               </div>
-              <h4>FOOD DELIVERY WEB APP</h4>
-              <span>Java & SQL</span>
-              <p>
-                A Java MVC and MySQL-based platform for users to order food,
-                rate restaurants.
-              </p>
-              <button
-                className="chip source"
-                onClick={() =>
-                  window.open(
-                    "https://github.com/sachinnimbal/Food-Delivery-Web-App",
-                    "_blank"
-                  )
-                }
-              >
-                Github
-              </button>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
             </div>
             <div className="card">
               <div className="card-image-container">
-                <img src={Invoice} alt="INVOICE MAKING WEB APP" />
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
               </div>
-              <h4>INVOICE MAKING WEB APP</h4>
-              <span>PHP</span>
-              <p>
-                A small invoicing application using a Bootstrap template, PHP
-                and MySQL.
-              </p>
-              <button
-                className="chip source"
-                onClick={() =>
-                  window.open(
-                    "https://github.com/sachinnimbal/INVOICE-MAKING-WEB-APP",
-                    "_blank"
-                  )
-                }
-              >
-                Github
-              </button>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
             </div>
             <div className="card">
               <div className="card-image-container">
-                <img src={SUK} alt="SUK ANDROID APP" />
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
               </div>
-              <h4>SUK ANDROID APP</h4>
-              <span>Java & Firebase</span>
-              <p>
-                Developed using Java and Firebase to support learning and exam
-                preparation.
-              </p>
-              <button
-                className="chip source"
-                onClick={() =>
-                  window.open(
-                    "https://github.com/sachinnimbal/SUKApp",
-                    "_blank"
-                  )
-                }
-              >
-                Github
-              </button>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
             </div>
             <div className="card">
               <div className="card-image-container">
-                <img src={CIE} alt="CIE ENTRY SYSTEM" />
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
               </div>
-              <h4>CIE ENTRY SYSTEM</h4>
-              <span>PHP</span>
-              <p>
-                Involved in the design and implementation of the user interface
-                using HTML, CSS, and Bootstrap.
-              </p>
-              <button
-                className="chip source"
-                onClick={() =>
-                  window.open("https://github.com/sachinnimbal/CIE", "_blank")
-                }
-              >
-                Github
-              </button>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
             </div>
             <div className="card">
               <div className="card-image-container">
-                <img src={HOSTEL} alt="HOSTEL MANAGEMENT SYSTEM" />
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
               </div>
-              <h4>HOSTEL MANAGEMENT SYSTEM</h4>
-              <span>VB.Net & MS Access</span>
-              <p>
-                Hostel Management System Project Using VB.Net 2010 & MS Access
-                2007.
-              </p>
-              <button
-                className="chip source"
-                onClick={() =>
-                  window.open(
-                    "https://github.com/sachinnimbal/HMS-PROJECT-SKN",
-                    "_blank"
-                  )
-                }
-              >
-                Github
-              </button>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
+            </div>
+            <div className="card">
+              <div className="card-image-container">
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
+              </div>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
+            </div>
+            <div className="card">
+              <div className="card-image-container">
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
+              </div>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
+            </div>
+            <div className="card">
+              <div className="card-image-container">
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
+              </div>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
+            </div>
+            <div className="card">
+              <div className="card-image-container">
+                <img
+                  src="https://via.placeholder.com/800x400"
+                  alt="Project Title"
+                />
+              </div>
+              <h4>Project Title</h4>
+              <span>Languages Used</span>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <button className="chip source">Github</button>
             </div>
           </div>
         </div>
