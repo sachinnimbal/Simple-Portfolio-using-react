4d172c46e14551b8993f8ffb4cab943b4121db8c
Sachin Nimbal <87244512+sachinnimbal@users.noreply.github.com>
2024-05-12 09:48:56 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,45 +6,39 @@ export default function Education() {
           <div className="label-container">
             <h1 className="title">Education History</h1>
           </div>
-          <div className>
-            <div className="education-section">
-              <div className="edu">
-                <h3>
-                  <i className="fa fa-graduation-cap" /> Education
-                </h3>
-                <ul>
-                  <li>
-                    <span>MCA - Master of Computer Applications</span>
-                    <small className="year">02/2022 - 09/2023</small>
-                    <small className="college">
-                      Sharnbasva University, Kalaburagi
-                    </small>
-                  </li>
-                  <li>
-                    <span>BCA - Bachelor of Computer Applications</span>
-                    <small className="year">07/2018 - 09/2021</small>
-                    <small className="college">
-                      Sharnbasva University, Kalaburagi
-                    </small>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <span>12th Board - PUC</span>
-                    <small className="year">04/2015 - 05/2018</small>
-                    <small className="college">
-                      Nalanda Ind. PU College, Kalaburagi
-                    </small>
-                  </li>
-                  <li>
-                    <span>10th Board - SSLC</span>
-                    <small className="year">04/2014 - 04/2015</small>
-                    <small className="college">
-                      Vinayaka Education Society, Shorapur
-                    </small>
-                  </li>
-                </ul>
-              </div>
+          <div className="education-section">
+            <div className="edu">
+              <h3>
+                <i className="fa fa-graduation-cap" /> Education
+              </h3>
+              <ul>
+                <li>
+                  <span>Postgraduate</span>
+                  <small className="year">20XX - 20XX</small>
+                  <small className="college">Demo University, CityName</small>
+                </li>
+                <li>
+                  <span>Undergraduate</span>
+                  <small className="year">20XX - 20XX</small>
+                  <small className="college">Demo University, CityName</small>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <span>Class 12</span>
+                  <small className="year">20XX - 20XX</small>
+                  <small className="college">
+                    Class 12 College Name, CityName
+                  </small>
+                </li>
+                <li>
+                  <span>Class 10</span>
+                  <small className="year">20XX - 20XX</small>
+                  <small className="college">
+                    Class 10 School Name, CityName
+                  </small>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
